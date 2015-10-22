9ea83c84fc731cc752ebe17e30075060b7997e01
Jim Symolon <jsymolon@gmail.com>
2015-10-22 16:34:37 -0400

    setup vars for data

--- armsim-elf-tests.js
+++ armsim-elf-tests.js
@@ -5,7 +5,7 @@ QUnit.test( "elf loader test", function( assert ) {
    ELF.curLine = -1; // nothing loaded/read yet or exhausted
    ELF.RawElfDataIdx = 0;
    ELF.ElfData = new Uint8Array(window.memory);
-   var testdata = "{\"0\":[127,69,76,70,1,1,1,0,0,0,0,0,0,0,0,0],\"16\":[2,0,40,0,1,0,0,0,116,128,0,0,52,0,0,0]}";
+   var testdata = "{\"0\":[127,69,76,70,1,1,1,0,0,0,0,0,0,0,0,0],\"16\":[2,0,40,0,1,0,0,0,116,128,0,0,52,0,0,0],\"32\":[]}";
    // basic method test
    var pjson = JSON.parse(testdata);
    assert.equal(JSON.stringify(pjson), testdata, "basic method");
@@ -13,16 +13,13 @@ QUnit.test( "elf loader test", function( assert ) {
    ELF.RawElfData = testdata;
    ELF.parseJSON();
    var odata = JSON.stringify(ELF.ElfData);
-   assert.equal(odata, testdata, "basic method in ELF");
 
    // see what's objects are generated, get the array from addr 16
-   var rdata = ELF.ElfData[16];
-   assert.equal(rdata[2], 40, "pull addr 16's 3rd byte");
+   assert.equal(ELF.ElfData[18], 40, "pull addr 16's 3rd byte");
 
    // test individual elements
-   var arr = ELF.ElfData[0]; // "address" - 0
-   assert.equal( arr[0], 0x7f, "data check idx 0");
-   assert.equal( arr[5], 0x01, "data check idx 5");
+   assert.equal(ELF.ElfData[0], 0x7f, "data check idx 0");
+   assert.equal(ELF.ElfData[5], 0x01, "data check idx 5");
    // see if reading byte from end exits cleanly
    for (i=0; i<16; i++) {
       x = ELF.readByte(i);
@@ -35,6 +32,7 @@ QUnit.test( "elf loader test", function( assert ) {
 
    // now do the big file
    ELF.init("http://localhost:3000/convert?file=/Users/jsymolon/arm/adc.elf&type=json"); // loads off of the URL
-   console.log(ELF);
-   assert.equal(ELF.e_type, "", "");
+   console.log("armsim-elf-tests elfData:"+elfData);
+   console.log("armsim-elf-tests elfData.e_type:"+elfData.e_type);
+   assert.equal(elfData.e_type, "", "");
 });
