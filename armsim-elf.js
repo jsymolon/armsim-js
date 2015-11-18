f0972a351f6cd8c95d432bce9e2364051615a365
Jim Symolon <jsymolon@gmail.com>
2015-11-18 12:30:30 -0500

    added program header decode added section type decode added function to load program from ELF to local memory

--- armsim-elf.js
+++ armsim-elf.js
@@ -1,5 +1,19 @@
 // Follows basic module pattern
 // http://www.adequatelygood.com/JavaScript-Module-Pattern-In-Depth.html
+
+// HDR Identification Indexes 
+var CONST_ELF_HDR_IDX = {
+    EI_MAG0: 0,
+    EI_MAG1: 1,
+    EI_MAG2: 2,
+    EI_MAG3: 3,
+    EI_CLASS: 4,
+    EI_DATA: 5,
+    EI_VERSION: 6,
+    EI_PAD: 7,
+    EI_NIDENT: 16
+};
+
 var CONST_ELF_TYPES = {
     ET_NONE: 0,
     ET_REL: 1,
@@ -8,6 +22,15 @@ var CONST_ELF_TYPES = {
     ET_CORE: 4,
     ET_LOPROC: 0xff00,
     ET_HIPROC: 0xffff,
+    PT_NULL: 0,
+    PT_LOAD: 1,
+    PT_DYNAMIC: 2,
+    PT_INTERP: 3,
+    PT_NOTE: 4,
+    PT_SHLIB: 5,
+    PT_PHDR: 6,
+    PT_LOPROC: 0x70000000,
+    PT_HIPROC: 0x7fffffff,
     Elf32_Addr: 4,
     Elf32_Half: 2,
     Elf32_Off: 4,
@@ -49,7 +72,7 @@ var CONST_ELF_ARCH = {
     EM_ARCH64: 183
 };
 
-var CONST_ELF_HDR = {
+var CONST_ELF_SEC_HDR = {
     SHT_NULL: 0,
     SHT_PROGBITS: 1,
     SHT_SYMTAB: 2,
@@ -75,7 +98,63 @@ var ELF = (function() {
    my.loaded = false, // nothing loaded/read yet or exhausted
    my.littleEndian = 0;
    my.lastAddress = 0;
+   my.sections = [];   // array of section objects
+   my.psections = [];   // array of section objects
+   my.numSections = 0; // number of sections loaded
 
+   // ------------------------------------------------------------
+   my.derefSecType = function(stype) {
+    if (stype === CONST_ELF_SEC_HDR.SHT_NULL) {
+      return "NULL  ";
+    }
+    if (stype === CONST_ELF_SEC_HDR.SHT_PROGBITS) {
+      return "PRGBTS";
+    }
+    if (stype === CONST_ELF_SEC_HDR.SHT_SYMTAB) {
+      return "SYMTAB";
+    }
+    if (stype === CONST_ELF_SEC_HDR.SHT_STRTAB) {
+      return "STRTAB";
+    }
+    if (stype === CONST_ELF_SEC_HDR.SHT_RELA) {
+      return "RELA  ";
+    }
+    if (stype === CONST_ELF_SEC_HDR.SHT_HASH) {
+      return "HASH  ";
+    }
+    if (stype === CONST_ELF_SEC_HDR.SHT_DYNAMIC) {
+      return "DYNA  ";
+    }
+    if (stype === CONST_ELF_SEC_HDR.SHT_NOTE) {
+      return "NOTE  ";
+    }
+    if (stype === CONST_ELF_SEC_HDR.SHT_NOBITS) {
+      return "NOBITS ";
+    }
+    if (stype === CONST_ELF_SEC_HDR.SHT_REL) {
+      return "REL   ";
+    }
+    if (stype === CONST_ELF_SEC_HDR.SHT_SHLIB) {
+      return "SHLIB   ";
+    }
+    if (stype === CONST_ELF_SEC_HDR.SHT_DYNSYM) {
+      return "DYNSYM";
+    }
+    if (stype === CONST_ELF_SEC_HDR.SHT_LOPROC) {
+      return "LOPROC";
+    }
+    if (stype === CONST_ELF_SEC_HDR.SHT_HIPROC) {
+      return "HIPROC";
+    }
+    if (stype === CONST_ELF_SEC_HDR.SHT_LOUSER) {
+      return "LOUSER";
+    }
+    if (stype === CONST_ELF_SEC_HDR.SHT_HIUSER) {
+      return "HIUSER";
+    }
+  };
+
+   // ------------------------------------------------------------
    my.parseJSON = function() {
     console.log("parseJSON");
     jsonData = JSON.parse(my.RawElfData);
@@ -101,6 +180,7 @@ var ELF = (function() {
     } while (elfData !== undefined);
    };
 
+   // ------------------------------------------------------------
    my.readByte = function(idx) {
       var item = my.ElfData[idx];
       //console.log("i:"+idx+" b:"+item);
@@ -121,35 +201,143 @@ var ELF = (function() {
       return longv;
    };
 
-   my.getValue = function (varname) {
-      console.log("getValue:"+varname+ " v:"+my[varname]);
-      return my[varname];
-   };
-
-   my.setValue = function (varname, value) {
-      console.log("setValue:"+varname+ " v:"+value);
-      my[varname] = value;
-   };
-
-   my.parseElfHeader = function () {
-     // 16 for eident
-     var hdrStr = "";
-     var i = 0;
-     for (i=1; i<4; i += 1) {
-        hdrStr += String.fromCharCode(my.readByte(i));
-     }
-     if (hdrStr !== "ELF") {
-        return false;
-     }
-     var index = 16;
-     my.setValue('e_type', my.readWord(index));
-     index += CONST_ELF_TYPES.Elf32_Half;
-     my.e_machine = my.readWord(index);
-     index += CONST_ELF_TYPES.Elf32_Half;
-     my.e_version = my.readLong(index);
+   // ------------------------------------------------------------
+   my.parseSection32 = function (i, index) {
+     var sec = {};
+     sec.sh_name = my.readLong(index);
      index += CONST_ELF_TYPES.Elf32_Word;
-     my.e_entry = my.readLong(index);
+     sec.sh_type = my.readLong(index);
+     index += CONST_ELF_TYPES.Elf32_Word;
+     sec.sh_flags = my.readLong(index);
+     index += CONST_ELF_TYPES.Elf32_Word;
+     sec.sh_addr = my.readLong(index);
      index += CONST_ELF_TYPES.Elf32_Addr;
+     sec.sh_offset = my.readLong(index);
+     index += CONST_ELF_TYPES.Elf32_Off;
+     sec.sh_size = my.readLong(index);
+     index += CONST_ELF_TYPES.Elf32_Word;
+     sec.sh_link = my.readLong(index);
+     index += CONST_ELF_TYPES.Elf32_Word;
+     sec.sh_info = my.readLong(index);
+     index += CONST_ELF_TYPES.Elf32_Word;
+     sec.sh_addralign = my.readLong(index);
+     index += CONST_ELF_TYPES.Elf32_Word;
+     sec.sh_entsize = my.readLong(index);
+     index += CONST_ELF_TYPES.Elf32_Word;
+     var stype = my.derefSecType(sec.sh_type);
+     console.log("["+i+"] "+sec.sh_name+" "+stype+" "+sec.sh_addr+" "+sec.sh_offset+" "+sec.sh_size+" "+sec.sh_entsize+" "+sec.sh_flags+" "+sec.sh_link+" "+sec.sh_info+" "+sec.sh_addralign);
+     return sec;
+   };
+
+   // ------------------------------------------------------------
+   my.parseSections = function () {
+     // read the section header
+     var index = my.e_shoff;
+     var nSec = my.e_shnum;
+     var secSize = my.e_shentsize;
+     console.log("Sections shoff:" + index + " num:" + nSec + " sz:" + secSize);
+     console.log("Section Headers");
+     console.log("[nr] Name   Type   Addr  Off  Size  ES  Flg Lk Inf Al");
+     for (i=0; i<nSec; i += 1) {
+       var sec = my.parseSection32(i, index);
+       my.sections.push(sec);
+       index = index + secSize;
+     }
+   };
+
+   // ------------------------------------------------------------
+   // symbol table
+   // st_name = word
+   // st_value = addr
+   // st_size = word
+   // st_info = char
+   // st_other = char
+   // st_shndx = half
+
+   // ------------------------------------------------------------
+   my.loadSymbols = function (sym_dict) {
+    // 1st index [0] is null, null termination strings
+    var numSec = my.sections.length;
+    for (var i = 0; i < numSec; i++) {
+      if (my.sections[i].e_type === CONST_ELF_SEC_HDR.SHT_STRTAB) {
+        // deref the name and if === ".strtab"
+        //
+        return {};
+      }
+    }
+   };
+
+   // ------------------------------------------------------------
+   my.loadProgHdr = function (i, index) {
+    var hdr = {};
+    hdr.p_type = my.readLong(index);
+    index += CONST_ELF_TYPES.Elf32_Word;
+    hdr.p_offset = my.readLong(index);
+    index += CONST_ELF_TYPES.Elf32_Off;
+    hdr.p_vaddr = my.readLong(index);
+    index += CONST_ELF_TYPES.Elf32_Addr;
+    hdr.p_paddr = my.readLong(index);
+    index += CONST_ELF_TYPES.Elf32_Addr;
+    hdr.p_filesz = my.readLong(index);
+    index += CONST_ELF_TYPES.Elf32_Word;
+    hdr.p_memsz = my.readLong(index);
+    index += CONST_ELF_TYPES.Elf32_Word;
+    hdr.p_flags = my.readLong(index);
+    index += CONST_ELF_TYPES.Elf32_Word;
+    hdr.p_align = my.readLong(index);
+    index += CONST_ELF_TYPES.Elf32_Word;
+    console.log("["+i+"] "+hdr.p_type+" "+hdr.p_offset+" "+hdr.p_vaddr+" "+hdr.p_paddr+" "+hdr.p_filesz+" "+hdr.p_memsz+" "+hdr.p_flags+" "+hdr.p_align);
+    return hdr;
+   };
+
+   // ------------------------------------------------------------
+   my.parseProgHdrs = function () {
+     // read the section header
+     var index = my.e_phoff;
+     var nHdr = my.e_phnum;
+     var pSize = my.e_phentsize;
+     console.log("Program Headers");
+     console.log("Type Offset VirtAddr PhyAddr FileSz MemSz F Align");
+     for (i=0; i<nHdr; i += 1) {
+       var hdr = my.loadProgHdr(i, index);
+       my.psections.push(hdr);
+       index = index + pSize;
+     }
+   };
+
+   // ------------------------------------------------------------
+   // called after parseElf and from parent w/ ptr to memory
+   my.loadProgBits = function (memory_arr) {
+    // load the section w/ progbits data
+    var numSec = my.sections.length;
+    var elfindx = 0;
+    var len = 0;
+    for (var i = 0; i < numSec; i++) {
+      if (my.sections[i].sh_type === CONST_ELF_SEC_HDR.SHT_PROGBITS) {
+        var ssec = my.sections[i];
+        elfindx = ssec.sh_offset;
+        len = ssec.sh_size;
+      }
+    }
+    // load the program section for data
+    numSec = my.psections.length;
+    for (var i = 0; i < numSec; i++) {
+      if (my.sections[i].e_type === CONST_ELF_SEC_HDR.PT_LOAD) {
+        var hdrsec = my.psections[i];
+        var prog_addr = hdrsec.p_paddr;
+        //console.log("PT_LOAD e ix:"+elfindx+" l:"+len+" paddr:"+prog_addr);
+        for (var j = prog_addr; j < prog_addr+len; j++) {
+          var b = my.ElfData[elfindx]; //my.readByte[elfindx];
+          memory_arr[j] = b;
+          //console.log("elfindx:"+elfindx+" j:"+j+" "+" = "+b);
+          elfindx = elfindx + 1;
+        }
+      }
+    }
+   };
+
+   // ------------------------------------------------------------
+   my.parseElfHdr32 = function (index) {
      my.e_phoff = my.readLong(index);
      index += CONST_ELF_TYPES.Elf32_Off;
      my.e_shoff = my.readLong(index);
@@ -168,9 +356,53 @@ var ELF = (function() {
      index += CONST_ELF_TYPES.Elf32_Half;
      my.e_shstrndx = my.readWord(index);
      index += CONST_ELF_TYPES.Elf32_Half;
+     return index;
+   };
+
+   my.parseElfHdr64 = function (index) {
+      console.log("No ELF HDR 64 done");
+      alert("No ELF HDR 64 done");
+      return index;
+   };
+
+   my.parseElfHeader = function () {
+     // 16 for eident
+     var hdrStr = "";
+     var i = 0;
+     for (i=1; i<4; i += 1) {
+        hdrStr += String.fromCharCode(my.readByte(i));
+     }
+     if (hdrStr !== "ELF") {
+        return false;
+     }
+     // header before 32/64 split
+     my.ei_class = my.readByte(CONST_ELF_HDR_IDX.EI_CLASS);
+     my.ei_data = my.readByte(CONST_ELF_HDR_IDX.EI_DATA);
+     my.ei_version = my.readByte(CONST_ELF_HDR_IDX.EI_VERSION);
+     var index = 16;
+     my.e_type = my.readWord(index);
+     index += CONST_ELF_TYPES.Elf32_Half;
+     my.e_machine = my.readWord(index);
+     index += CONST_ELF_TYPES.Elf32_Half;
+     my.e_version = my.readLong(index);
+     index += CONST_ELF_TYPES.Elf32_Word;
+     my.e_entry = my.readLong(index);
+     index += CONST_ELF_TYPES.Elf32_Addr;
+     if (my.ei_data === CONST_ELF_CLASS.E_CLASS32) {
+       console.log("HDR ei_data is 32 bit");
+       index = my.parseElfHdr32(index);
+     } else {
+       console.log("HDR ei_data is 64 bit");
+       index = my.parseElfHdr64(index);
+     }
+     if (my.e_phoff != 0) {
+       my.parseProgHdrs();
+     }
+     my.parseSections();
      return true;
    };
 
+   // ------------------------------------------------------------
    my.parseElf = function () {
       console.log("parseElf called");
       var ElfOK = my.parseElfHeader(0);  // idx = 0 for start of file
@@ -180,6 +412,12 @@ var ELF = (function() {
       return true;
    };
 
+   // ------------------------------------------------------------
+   my.dumpSections = function () {
+      console.log("dumpSections called");
+    };
+
+   // ------------------------------------------------------------
    my.init = function (url) {
       my.ElfData = [];
       my.RawElfData = [];
